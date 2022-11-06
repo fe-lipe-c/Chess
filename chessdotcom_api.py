81f4120f9f34b5ce010a1528d361b6895b5dff01
Felipe Costa <felipecosta8449@hotmail.com>
2022-11-06 08:09:07 -0300

    first commit

---
+++ chessdotcom_api.py
@@ -0,0 +1,43 @@
+"""Some class and functions to get data from chess.com API."""
+
+import pandas as pd
+import requests
+
+
+class Player_Profile:
+    """Player profile class."""
+
+    def __init__(self, username):
+        """Initialize."""
+        self.username = username
+        self.profile = self.player_profile()
+        self.stats = self.player_stats()
+        self.online = self.isonline()
+
+    def player_profile(self):
+        """Get player profile."""
+        url = "https://api.chess.com/pub/player/" + self.username
+        response = requests.get(url).json()
+        return pd.json_normalize(response)
+
+    def player_stats(self):
+        """Get player stats."""
+        url = "https://api.chess.com/pub/player/" + self.username + "/stats"
+        response = requests.get(url).json()
+        return pd.json_normalize(response)
+
+    def isonline(self):
+        """Check if player is online."""
+        url = "https://api.chess.com/pub/player/" + self.username + "/is-online"
+        response = requests.get(url).json()
+        return response["online"]
+
+
+def titled_players(self, title):
+    """Get titled players.
+
+    Valid title abbreviations are: GM, WGM, IM, WIM, FM, WFM, NM, WNM, CM, WCM.
+    """
+    url = "https://api.chess.com/pub/titled/" + title
+    response = requests.get(url).json()
+    return pd.DataFrame(response["players"], columns=["username"])
---
+++ notebook.py
@@ -0,0 +1,7 @@
+"""Notebook with exploration of chess.com api."""
+
+import chessdotcom_api
+
+username = "fe_lipe_br"
+
+chessdotcom_api.Player_Profile(username).player_stats()
